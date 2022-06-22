e8566fc402d22d320da6014f07620990b8a220af
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-06-21 22:33:31 -0300

    Add handler for create user

---
+++ SampleCQRS/src/Application/CommandHandlers/CreateUserHandler.cs
@@ -0,0 +1,15 @@
+﻿using Application.Commands;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.CommandHandlers
+{
+    public class CreateUserHandler : IRequestHandler<CreateUserCommand, User>
+    {
+        public Task<User> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            request.User.Id = Guid.NewGuid();
+            return Task.FromResult(request.User);
+        }
+    }
+}
---
+++ SampleCQRS/src/Application/Commands/CreateUserCommand.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using MediatR;
+
+namespace Application.Commands
+{
+    public class CreateUserCommand : IRequest<User>
+    {
+        public CreateUserCommand(User user)
+        {
+            User = user;
+        }
+
+        public User User { get; set; }
+    }
+}
