c4240bad308e47842cd11c2392247b3479601048
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-06-21 22:32:00 -0300

    Add request and response

---
+++ SampleCQRS/src/Api/Models/Request/CreateUserRequest.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+
+namespace Api.Models.Request
+{
+    public class CreateUserRequest
+    {
+        public string Name { get; set; }
+        public string Document { get; set; }
+
+        public static implicit operator User(CreateUserRequest request)
+        {
+            if(request == null) 
+                return null;
+
+            return new User
+            {
+               Document = request.Document,
+               Name = request.Name
+            };
+        }
+    }
+}
---
+++ SampleCQRS/src/Api/Models/Response/UserResponse.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+
+namespace Api.Models.Response
+{
+    public class UserResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Document { get; set; }
+
+        public static implicit operator UserResponse(User user)
+        {
+            if (user == null)
+                return null;
+
+            return new UserResponse
+            {
+                Document = user.Document,
+                Name = user.Name,
+                Id = user.Id
+            };
+        }
+    }
+}
