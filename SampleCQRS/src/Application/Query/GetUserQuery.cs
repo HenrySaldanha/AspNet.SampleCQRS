397befe5f78b2e82c0aa8fe947b7977ef68ad106
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-06-21 22:34:17 -0300

    Add handler for get user

---
+++ SampleCQRS/src/Application/Query/GetUserQuery.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using MediatR;
+
+namespace Application.Commands
+{
+    public class GetUserQuery : IRequest<User>
+    {
+        public GetUserQuery(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; set; }
+    }
+}
---
+++ SampleCQRS/src/Application/QueryHandler/GetUserHandler.cs
@@ -0,0 +1,21 @@
+﻿using Application.Commands;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Handlers
+{
+    public class GetUserHandler : IRequestHandler<GetUserQuery, User>
+    {
+        public Task<User> Handle(GetUserQuery request, CancellationToken cancellationToken)
+        {
+            var user = new User
+            {
+                Document = "12312312312",
+                Id = Guid.NewGuid(),
+                Name = "Full Name"
+            };
+
+            return Task.FromResult(user);
+        }
+    }
+}
