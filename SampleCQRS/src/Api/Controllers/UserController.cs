376913f210b91078a29131dfbb400340e36aefbc
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-06-21 22:31:31 -0300

    Add user controller

---
+++ SampleCQRS/src/Api/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Api.Models.Request;
+using Api.Models.Response;
+using Application.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("user")]
+    [Produces("application/json")]
+    public class UserController : ControllerBase
+    {
+        /// <summary>
+        /// Create a new user
+        /// </summary>
+        /// <param name="userRequest"> Represents the new user to be created </param>
+        [HttpPost]
+        [ProducesResponseType(typeof(UserResponse), StatusCodes.Status201Created)]
+        public async Task<ActionResult> CreateUserAsync(
+            [FromServices] IMediator _mediator,
+            [FromBody] CreateUserRequest userRequest)
+        {
+            UserResponse user = await _mediator.Send(new CreateUserCommand(userRequest));
+            return Created(nameof(CreateUserAsync), user);
+        }
+
+        /// <summary>
+        /// Get user by uuid
+        /// </summary>
+        /// <param name="id"> Represents the user unique identifier </param>
+        [HttpGet]
+        [ProducesResponseType(typeof(int), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(UserResponse), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetByIdAsync(
+            [FromServices] IMediator _mediator,
+            [FromQuery] Guid id)
+        {
+            if (id == Guid.Empty)
+                return BadRequest();
+
+            UserResponse result = await _mediator.Send(new GetUserQuery(id));
+            return Ok(result);
+        }
+    }
+}
